291ba6c7f52a981f39973f74a872b9bcaf8a61bb
Michael Harris <mhguitar1997@gmail.com>
2019-07-08 13:05:20 -0700

    Home touchdown click event add

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ function App() {
       <section className="buttons">
         <div className="homeButtons">
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button className="homeButtons__touchdown">Home Touchdown</button>
+          <button className="homeButtons__touchdown" onClick={() => changeHome(6)}>Home Touchdown</button>
           <button className="homeButtons__fieldGoal">Home Field Goal</button>
         </div>
         <div className="awayButtons">
