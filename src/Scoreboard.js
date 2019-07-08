b1d20a0ae3320152648661aad9335295daf74ad6
Michael Harris <mhguitar1997@gmail.com>
2019-07-08 13:56:08 -0700

    Pass home and away score as props to scoreboard

--- src/Scoreboard.js
+++ src/Scoreboard.js
@@ -1,12 +1,26 @@
+import React from "react";
+import BottomRow from "./BottomRow";
 
-import React from 'react';
+const Scoreboard = (props) => {
+  return (
+    <section className="scoreboard">
+      <div className="topRow">
+        <div className="home">
+          <h2 className="home__name">Lions</h2>
 
-const Scoreboard = () => {
-    return (
-        <div>
-            
+          {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
+
+          <div className="home__score">{props.homeScore}</div>
         </div>
-    );
+        <div className="timer">00:03</div>
+        <div className="away">
+          <h2 className="away__name">Tigers</h2>
+          <div className="away__score">{props.awayScore}</div>
+        </div>
+      </div>
+      <BottomRow />
+    </section>
+  );
 };
 
 export default Scoreboard;
