f72ff361d4114059fb56ded423e4bd87801f4b5b
Michael Harris <mhguitar1997@gmail.com>
2019-07-08 13:15:21 -0700

    Field goal score change for home team

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ function App() {
         <div className="homeButtons">
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
           <button className="homeButtons__touchdown" onClick={() => changeHome(6)}>Home Touchdown</button>
-          <button className="homeButtons__fieldGoal">Home Field Goal</button>
+          <button className="homeButtons__fieldGoal" onClick={() => changeHome(3)}>Home Field Goal</button>
         </div>
         <div className="awayButtons">
           <button className="awayButtons__touchdown">Away Touchdown</button>
