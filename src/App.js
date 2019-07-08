43065fa26ac337a87c6429eea964f871e03c237c
Michael Harris <mhguitar1997@gmail.com>
2019-07-08 13:18:28 -0700

    Touchdown now worth 7 points instaead of 6

--- src/App.js
+++ src/App.js
@@ -32,11 +32,11 @@ function App() {
       <section className="buttons">
         <div className="homeButtons">
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button className="homeButtons__touchdown" onClick={() => changeHome(6)}>Home Touchdown</button>
+          <button className="homeButtons__touchdown" onClick={() => changeHome(7)}>Home Touchdown</button>
           <button className="homeButtons__fieldGoal" onClick={() => changeHome(3)}>Home Field Goal</button>
         </div>
         <div className="awayButtons">
-          <button className="awayButtons__touchdown" onClick={() => changeAway(6)}>Away Touchdown</button>
+          <button className="awayButtons__touchdown" onClick={() => changeAway(7)}>Away Touchdown</button>
           <button className="awayButtons__fieldGoal" onClick={() => changeAway(3)}>Away Field Goal</button>
         </div>
       </section>
