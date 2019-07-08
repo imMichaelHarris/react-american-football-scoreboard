7ccb621437f6d34995ba3360077639a7f02a39be
Michael Harris <mhguitar1997@gmail.com>
2019-07-08 14:09:36 -0700

    Fill buttons component

--- src/App.js
+++ src/App.js
@@ -15,37 +15,7 @@ function App() {
   return (
     <div className="container">
       <Scoreboard homeScore={homeScore} awayScore={awayScore} />
-      <section className="buttons">
-        <div className="homeButtons">
-          {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button
-            className="homeButtons__touchdown"
-            onClick={() => changeHome(7)}
-          >
-            Home Touchdown
-          </button>
-          <button
-            className="homeButtons__fieldGoal"
-            onClick={() => changeHome(3)}
-          >
-            Home Field Goal
-          </button>
-        </div>
-        <div className="awayButtons">
-          <button
-            className="awayButtons__touchdown"
-            onClick={() => changeAway(7)}
-          >
-            Away Touchdown
-          </button>
-          <button
-            className="awayButtons__fieldGoal"
-            onClick={() => changeAway(3)}
-          >
-            Away Field Goal
-          </button>
-        </div>
-      </section>
+
     </div>
   );
 }
