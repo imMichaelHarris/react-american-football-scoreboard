47ff7a1f0b005ed02182984e5106702707f40596
Michael Harris <mhguitar1997@gmail.com>
2019-07-08 14:43:09 -0700

    Add buttons classname to parent div in Buttons.js

--- src/Buttons.js
+++ src/Buttons.js
@@ -2,7 +2,8 @@ import React from "react";
 
 const Buttons = (props) => {
   return (
-    <>
+    <div className="buttons">
+    <button>Change Quarter</button>
       <section className="buttons">
         <div className="homeButtons">
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
@@ -34,7 +35,7 @@ const Buttons = (props) => {
           </button>
         </div>
       </section>
-    </>
+    </div>
   );
 };
 
