73417b41f45906891adcd97763c6cbfa581a7a2e
Michael Harris <mhguitar1997@gmail.com>
2019-07-08 17:21:46 -0700

    Remove old code

--- src/App.js
+++ src/App.js
@@ -6,22 +6,16 @@ import Scoreboard from "./Scoreboard";
 import Buttons from "./Buttons";
 
 function App() {
-  //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
-  const [homeScore, setHomeScore] = useState(0);
-  const [awayScore, setAwayScore] = useState(0);
   const [scores, setScore] = useState({
     home: 0,
     away: 0
   });
   const [quarter, setQuarter] = useState([1, 2, 3, 4]);
 
-  // const changeHome = points => setHomeScore(homeScore + points);
-  // const changeAway = points => setAwayScore(awayScore + points);
-
   const changeScore = (team, points) =>
     team === "Home"
-      ? setScore({...scores, home: scores.home + points})
-      : setScore({ ...scores, away: scores.away + points});
+      ? setScore({ ...scores, home: scores.home + points })
+      : setScore({ ...scores, away: scores.away + points });
 
   const changeQuarter = () => setQuarter(quarter + 1);
   return (
