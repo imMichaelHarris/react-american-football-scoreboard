65ffa3103dc5b494f4c72a15b040d559a908067a
Michael Harris <mhguitar1997@gmail.com>
2019-07-08 17:06:19 -0700

    Create hook that has both home and away scores

--- src/App.js
+++ src/App.js
@@ -9,6 +9,10 @@ function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
   const [homeScore, setHomeScore] = useState(0);
   const [awayScore, setAwayScore] = useState(0);
+  const { score, setScore } = useState({
+    home: 0,
+    away: 0
+  });
   const [quarter, setQuarter] = useState([1, 2, 3, 4]);
 
   // const changeHome = points => setHomeScore(homeScore + points);
@@ -19,10 +23,14 @@ function App() {
       ? setHomeScore(homeScore + points)
       : setAwayScore(awayScore + points);
 
-  const changeQuarter = (number) => setQuarter(quarter[number + 1])
+  const changeQuarter = () => setQuarter(quarter + 1);
   return (
     <div className="container">
-      <Scoreboard homeScore={homeScore} awayScore={awayScore} quarter={quarter}/>
+      <Scoreboard
+        homeScore={homeScore}
+        awayScore={awayScore}
+        quarter={quarter}
+      />
       <Buttons changeScore={changeScore} changeQuarter={changeQuarter} />
     </div>
   );
